82797ebb9c7d6f959b69b7d5df02859ae515b114
YuriyBeregovoy <rik0681@gmail.com>
2023-08-04 19:25:28 +0300

    added import user.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import { Profile } from "./Profile/Profile";
-import userData from "../components/Profile/"
+import userData from "./Profile/user.json"
 
 
 export const App = () => {
+ 
   return (
     <div>
       <Profile items={userData}/>
     </div>
   );
 };
+
